1db4bed0de2256600d0ad519d670483f60c357ae
Ayush gupta <91381231+Ayush150655@users.noreply.github.com>
2023-09-02 23:19:54 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (58.81%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> ans (n , vector<int>(n));
+        int c = 1;
+        for(int l = 0 ; l < (n+1)/2 ; l++)
+        {
+            for(int i = l ; i < (n-l) ; i++)
+            {
+                ans[l][i] = c++;
+            }
+            for(int i = l+1 ; i <= n-l-1 ; i++)
+            {
+                ans[i][n-l-1] = c++;
+            }
+            for(int i = n-l-2 ; i >= l ; i--)
+            {
+                ans[n-l-1][i] = c++;
+            }
+            for(int i = n-l-2 ; i > l ; i--)
+            {
+                ans[i][l] = c++;
+            }
+        }
+        return ans;
+    }
+};
